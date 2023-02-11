c497a687acca9a4ba99b2ca58200ab7fdeec7e36
Lokpati Tiwari <36004515+seen2@users.noreply.github.com>
2023-02-11 18:50:46 +0000

    Tech Explain Comp

---
+++ src/components/TechExplained.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import Image, { StaticImageData } from "next/image";
+
+
+import WebDevBanner from "../../public/WebDev.jpg";
+import AppDevBanner from "../../public/AppDev.jpg";
+import StyledBordered from './StyledBordered';
+// import SolutionsBanner from "../../public/Solutions.jpg";
+
+
+export default function TechExplained(props: { title: string, content?: string, price?: number, image?: StaticImageData,feaures:string[] }) {
+  return (
+    <div className='container' >
+      <div className="d-flex justify-content-center" >
+        <StyledBordered><h2 className='h2 text-center text-info'>{props.title || "Web Development"}</h2></StyledBordered>
+      </div>
+      <br />
+      <div className="d-flex flex-row flex-wrap justify-content-around rounded">
+        <Image
+          src={props.image || WebDevBanner}
+          className="img-fluid"
+
+          // unoptimized
+          quality={90}
+          alt="Picture of the author"
+        />
+        <p style={{ margin: "3%" }} className="lead">
+          {props.content}
+        </p>
+
+      </div>
+        <StyledBordered><h3 className="h6 text-success">RS-{props.price || "NA"}</h3></StyledBordered>
+        <br/>
+        <br/>
+
+
+      <p className="text-muted">
+        NOTE: All prices are excluding any taxes applicable by state or central government.
+         Further Customization or enhancement will increase the cost and reducing the feature will reduce cost.
+      </p>
+        <div className="d-flex flex-row flex-wrap justify-content-around">
+          {props.feaures.map(feature=>(<p className="h3 text-center"><li>{feature}</li></p>))}
+        
+        </div>
+      <u>All rights reserved @2023 Antatrix</u>
+
+      <hr />
+    </div>
+  )
+}
