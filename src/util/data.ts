7f3545575c3b4925b80a2518ba6b09b5975a0e4c
Lokpati Tiwari <36004515+seen2@users.noreply.github.com>
2023-02-11 18:48:12 +0000

    Data for user

---
+++ src/util/data.ts
@@ -0,0 +1,155 @@
+
+
+import WordpressLogo from "../../public/wordpress.png";
+import WebDevLogo from "../../public/WebDev.jpg";
+import NativeDevLogo from "../../public/Native-Development.jpg";
+import AppDevLogo from "../../public/AppDev.jpg";
+import SoftwareDevLogo from "../../public/web-development.png";
+import MenLogo from "../../public/man.png";
+import WomenLogo from "../../public/women.png";
+
+
+
+export const services = [
+  {
+    title: "Website",
+    images: WebDevLogo,
+    features: [
+      "User can Login",
+      "User Can modify their personal Data",
+      "User Can View Public Data",
+      "API Based",
+      "SEO Friendly",
+      "Scalable",
+      "Responsive",
+      "10 Different Dynamic Pages",
+      "Admin Pannel",
+      "Can be Ecomerce store or personal",
+      "6 months Free Maintainence"
+    ],
+    price: 1000000,
+    content: "In Full-stack Web Development, We will develop API base backend and Responsive fast reloading SEO Friendly Front-end that will be compatible with all screen sizes, Users can log in, User Can modify their personal Data, User Can View, Public Data, API Based, SEO Friendly, and Scalable with 10 Different Dynamic Pages.Modifications can be done as per the requirements and costs might increase for the same."
+  },
+  {
+    title: "Website and Native Mobile App",
+    images: AppDevLogo,
+    features: [
+      "User can Login",
+      "User Can modify their personal Data",
+      "User Can View Public Data",
+      "API Based",
+      "Scalable",
+      "SEO Friendly",
+      "Responsive",
+      "10 Different Dynamic Pages",
+      "Admin Pannel",
+      "Can be Ecomerce store or personal",
+      "All feature of Wesite in Native Mobile Application",
+      "Native App for both ios & android",
+      "6 months Free Maintainence"
+    ],
+    price: 170000,
+    content: "In Full-stack Development, We will develop API base backend and Responsive fast reloading SEO Friendly Web and Native Application Front-end that will be compatible with all screen sizes, Users can log in, User Can modify their personal Data, User Can View, Public Data, API Based, and Scalable with 10 Different Dynamic Pages.\nAll the features of the website will be available in Native Mobile Applications.\nModifications can be done as per the requirements and costs might increase for the same."
+  },
+  {
+    title: "Mobile App",
+    images: NativeDevLogo,
+    features: [
+      "User can Login",
+      "User Can modify their personal Data",
+      "User Can View Public Data",
+      "API Based",
+      "Scalable",
+      "Responsive",
+      "Admin Pannel",
+      "Can be Ecomerce store or personal",
+      "10 Different Dynamic Pages",
+      "Native App for both ios & android",
+      "6 months Free Maintainence"
+
+    ],
+    price: 100000,
+    content: "In Full-stack Mobile Development, We will develop API base backend and Responsive fast reloading SEO Friendly Front-end that will be compatible with all screen sizes, Users can log in, User Can modify their personal Data, User Can View, Public Data, API Based, and Scalable with 10 Different Dynamic Pages.\nModifications can be done as per the requirements and costs might increase for the same."
+
+  },
+  {
+    title: "Wordpress Website",
+    images: WordpressLogo,
+    features: [
+      "User can Login",
+      "User Can modify their personal Data",
+      "SEO Friendly",
+      "User Can View Public Data",
+      "Admin Pannel",
+      "Can be Ecomerce store or personal",
+      "6 months Free Maintainence"
+    ],
+    price: 70000,
+    content: "In Full-stack Web Development, We will develop API base backend and Responsive fast reloading SEO Friendly Front-end that will be compatible with all screen sizes, Users can log in, User Can modify their personal Data, User Can View, Public Data, API Based, SEO Friendly, with 10 Different Dynamic Pages.Modifications can be done as per the requirements and costs might increase for the same."
+
+  },
+  {
+    title: "Perssonal Software Development",
+    images: SoftwareDevLogo,
+    features: [
+      "Requirement Based",
+      "All features will be desgined based on requirement",
+      "Prices will be based on requirement",
+    ],
+    price: undefined,
+    content: ""
+  }
+];
+
+export const teamData=[
+  {
+    name:"Sintu Tiwari",
+    image:MenLogo,
+    gender:"M",
+    description:"Full Stack Developer",
+    designation:"Developer"
+  },
+  {
+    name:"Ankit Singh",
+    image:MenLogo,
+    gender:"M",
+    description:"Responsible for in UI, UX and Graphic Desgining",
+    designation:"Graphic Desginer"
+  },
+  {
+    name:"Nidhi Singh",
+    image:WomenLogo,
+    gender:"F",
+    description:"Responsible for Digital Marketing",
+    designation:"Social/Digital Marketing Manager"
+  }
+  ,
+  {
+    name:"Bimal Tiwari",
+    image:MenLogo,
+    gender:"M",
+    description:"Responsible for Marketing Cordination Quality Analysis & Requirement Gathering",
+    designation:"Scrum Master"
+  },
+  {
+    name:"Deepa Shah",
+    image:WomenLogo,
+    gender:"F",
+    description:"Responsible for in UI, UX and Graphic Desgining",
+    designation:"Graphic Desginer"
+  },
+  {
+    name:"Abhay Ojha",
+    image:MenLogo,
+    gender:"M",
+    description:"Database desgining",
+    designation:"Database Engineer"
+  },
+  {
+    name:"Vivek Dubey",
+    image:MenLogo,
+    gender:"M",
+    description:"Developer",
+    designation:"Full Stack Developer"
+  }
+]
