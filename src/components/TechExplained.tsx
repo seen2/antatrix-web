2375a3512f37c55acd066a74794cb4d8ea6ab092
Lokpati Tiwari <36004515+seen2@users.noreply.github.com>
2023-02-14 19:03:11 +0000

    Pricing UI improved

--- src/components/TechExplained.tsx
+++ src/components/TechExplained.tsx
@@ -8,11 +8,11 @@ import StyledBordered from './StyledBordered';
 // import SolutionsBanner from "../../public/Solutions.jpg";
 
 
-export default function TechExplained(props: { title: string, content?: string, price?: number, image?: StaticImageData,feaures:string[] }) {
+export default function TechExplained(props: { title: string, content?: string, price?: number, image?: StaticImageData, feaures: string[] }) {
   return (
     <div className='container' >
       <div className="d-flex justify-content-center shadow-lg p-3 mb-5 bg-body-tertiary rounded border-start border-end" >
-      <h2 className='h2 text-center text-info'>{props.title || "Web Development"}</h2>
+        <h2 className='display-3 text-center text-info'>{props.title || "Web Development"}</h2>
       </div>
       <br />
       <div className="d-flex flex-row flex-wrap justify-content-around  shadow-lg p-3 mb-5 bg-body-tertiary rounded">
@@ -29,22 +29,24 @@ export default function TechExplained(props: { title: string, content?: string, 
         </p>
 
       </div>
-        <StyledBordered><h3 className="h6 text-success">RS-{props.price || "NA"}</h3></StyledBordered>
-        <br/>
-        <br/>
+      <StyledBordered><h3 className="h6 text-success">RS-{props.price + "*" || "NA"}</h3></StyledBordered>
+      <br />
+      <br />
 
 
       <p className="text-muted">
         NOTE: All prices are excluding any taxes applicable by state or central government.
-         Further Customization or enhancement will increase the cost and reducing the feature will reduce cost.
+        Further Customization or enhancement will increase the cost and reducing the feature will reduce cost.
       </p>
-    <p className="text-info h1">Inside:</p>
-        <div className="d-block justify-content-center">
-          {props.feaures.map((feature,index)=>(<p key={index+feature+""} className="h2 border-top border-end border-start shadow-lg p-3 mb-5 bg-body-tertiary rounded border-info text-center">{feature}</p>))}
-        
-        </div>
-      <u>All rights reserved @2023 Antatrix</u>
+      <p className="text-info h1">Inside:</p>
+      <div className="d-block justify-content-center">
+        {props.feaures.map((feature, index) => (<p key={index + feature + ""} className="h2 border-top border-end border-start shadow-lg p-3 mb-5 bg-body-tertiary rounded border-info text-center">{feature}</p>))}
 
+      </div>
+      <div className='rounded ' style={{height:50}}>
+        <u className='text-muted '  >All rights reserved @2023 Antatrix</u>
+
+      </div>
 
     </div>
   )
